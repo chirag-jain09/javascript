f1c9bae3a57e0e967e3fafb9cf6dd2662def5830
Chirag Jain <chiragj2910@gmail.com>
2024-10-14 18:01:03 +0530

    Create 02-datatypes.js

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,23 @@
+// datatypes
+
+// string, number, boolean, null, undefined, symbol 
+
+let name = "chirag"
+let age = 10;
+let isLoggedIn = true
+let status = null
+let area;
+let sym = Symbol("abc");
+
+// symbols are unique
+console.log(typeof "name");      //string
+console.log(typeof age);         //number
+console.log(typeof isLoggedIn);  //boolean -> can be true or false
+console.log(typeof area);        //undefined
+console.log(typeof null);        //object
+console.log(typeof sym);         //symbol
+console.log(typeof undefined);   //undefined
+
+
+
+
