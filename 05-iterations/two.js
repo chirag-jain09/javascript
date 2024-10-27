cb3c9a7c44f0ccd691c78037bd0d194e421636d8
Chirag Jain <chiragj2910@gmail.com>
2024-10-27 16:03:46 +0530

    Create two.js

---
+++ 05-iterations/two.js
@@ -0,0 +1,30 @@
+// while loop
+
+// while (condition) {
+    
+// }
+
+// let index = 1
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// do-while loop
+// do {
+    
+// } while (condition);
+
+// let score = 10
+// do {
+//     console.log(`score is ${score}`)
+//     score++
+// } while (score <= 20);
+
+let score = 10
+do {
+    console.log(`score is ${score}`)        // score is 10
+    score++
+} while (score < 10);
+
+// in do while loop the loop will run atleast once
