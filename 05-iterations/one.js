6d57c0bd91c3fc689fc011237329314e8724a4d8
Chirag Jain <chiragj2910@gmail.com>
2024-10-27 16:03:40 +0530

    Create one.js

---
+++ 05-iterations/one.js
@@ -0,0 +1,31 @@
+// for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];    
+// }
+
+// nested loops
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop value: ${i}`)    
+//     for (let j = 0; j < 10; j++) {
+//     console.log(`Inner Loop value: ${j} and inner loop ${i}`)        
+//     }
+// }
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5){
+//         console.log(`5 detected`);
+//         break;
+//     }
+//     console.log(`Value of i is: ${i}`)    
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i == 5){
+//         console.log(`5 detected`);
+//         continue;
+//     }
+//     console.log(`Value of i is: ${i}`)    
+// }
